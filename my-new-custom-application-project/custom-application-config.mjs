27be577978dfa15a7f6021b557abb65b8b71e0af
rajapandi-valtech <rajapandi.k@valtech.com>
2025-12-12 12:04:07 +0530

    config fixed

--- my-new-custom-application-project/custom-application-config.mjs
+++ my-new-custom-application-project/custom-application-config.mjs
@@ -1,19 +1,26 @@
-import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
+
+const entryPointUriPath = '${env:ENTRY_POINT_URI_PATH}';
+export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
+
 
 /**
  * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
  */
 const config = {
   name: 'Poc Rp',
-  entryPointUriPath,
-  cloudIdentifier: 'gcp-eu',
+  entryPointUriPath: '${env:ENTRY_POINT_URI_PATH}',
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
     development: {
       initialProjectKey: 'demo-valtech-b2c-2511cc',
     },
     production: {
-      applicationId: 'TODO',
-      url: 'https://your_app_hostname.com',
+     
+ applicationId: '${env:CUSTOM_APPLICATION_ID}',
+      url: '${env:APPLICATION_URL}'
+
     },
   },
   oAuthScopes: {
--- my-new-custom-application-project/src/constants.ts
+++ my-new-custom-application-project/src/constants.ts
@@ -1,6 +1,13 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'poc-rp';
+
+// In dev, mc-scripts will set process.env variables; in production,
+// the MC runtime injects window.app.* values.
+export const entryPointUriPath: string =
+  typeof window === 'undefined'
+    ? (process.env.ENTRY_POINT_URI_PATH as string)
+    : (window as any).app.entryPointUriPath;
+
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
